70bf94143862848d510253f1549383ab4d1c3190
cs13engineer <75882819+cs13engineer@users.noreply.github.com>
2022-12-27 00:24:53 +0530

    Update fsModule.js

--- File System/fsModule.js
+++ File System/fsModule.js
@@ -1,5 +1,9 @@
 // File system using Node
 
 const fs = require('fs')
+const path = require('path')
 
-console.log('file System: ',fs.open())
+var currentDirectory = path.join(__dirname)
+var currentWorkingDirectory = path.join(__dirname,'./Data/')
+
+console.log(`my current Directory is: ${currentDirectory}-${currentWorkingDirectory}`);
